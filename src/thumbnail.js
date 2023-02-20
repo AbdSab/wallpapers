d6e24c0f1354147c3b89c20439b8e21e080a38c2
Abdellah Sabiri <abdellah.sabiri.1995@gmail.com>
2023-02-20 12:30:05 +0100

    images thumb

--- src/thumbnail.js
+++ src/thumbnail.js
@@ -1,16 +1,15 @@
 const sharp = require('sharp');
 const fs = require('fs');
 const test = async () => {
-    const files = fs.readdirSync('../data/nature/full');
+    const files = fs.readdirSync('data/nature/full');
     for(let file of files) {
-        await sharp('../data/nature/full/' + file)
+        await sharp('data/nature/full/' + file)
             .resize({
                 width: 300,
                 height: 400,
-                fit: sharp.fit.cover
             })
             .sharpen()
-            .toFile('../data/nature/thumb/'+file)
+            .toFile('data/nature/thumb/'+file)
             .then(info => {
                 console.log(info);
             })
